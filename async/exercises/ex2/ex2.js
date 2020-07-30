a2c2e9faad0a932db9912bc0ca69f44adbfa35fa
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-29 18:22:28 -0700

    solve exercise 2

--- async/exercises/ex2/ex2.js
+++ async/exercises/ex2/ex2.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -19,9 +19,38 @@ function output(text) {
 
 // **************************************
 
+function makeThunk(fn, ...args) {
+	const val = fn(...args);
+	return function (cb) {
+		return cb(val);
+	};
+}
+
 function getFile(file) {
-	// what do we do here?
+	let text, fn;
+	fakeAjax(file, response => {
+		if (fn) fn(response);
+		else text = response;
+	});
+
+	return function (cb) {
+		if (text) cb(text);
+		else fn = cb;
+	};
 }
 
 // request all files at once in "parallel"
-// ???
+const thunk1 = getFile("file1");
+const thunk2 = getFile("file2");
+const thunk3 = getFile("file3");
+
+thunk1(contents => {
+	output(contents);
+	thunk2(contents => {
+		output(contents);
+		thunk3(contents => {
+			output(contents);
+			output("Complete!");
+		});
+	});
+});
