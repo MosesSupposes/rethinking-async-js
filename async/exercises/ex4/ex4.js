e9944b8ae1960f6dae0f5c46a900195c65608f27
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-30 16:36:17 -0700

    complete exercise 4

--- async/exercises/ex4/ex4.js
+++ async/exercises/ex4/ex4.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -21,8 +21,8 @@ function output(text) {
 // The old-n-busted callback way
 
 function getFile(file) {
-	return new Promise(function(resolve){
-		fakeAjax(file,resolve);
+	return new Promise(function (resolve) {
+		fakeAjax(file, resolve);
 	});
 }
 
@@ -32,5 +32,8 @@ function getFile(file) {
 // Render as each one finishes,
 // but only once previous rendering
 // is done.
-
-// ???
+["file1", "file2", "file3"]
+	.map(getFile)
+	.reduce(function combine(chain, promise) {
+		return chain.then(() => promise).then(output);
+	}, Promise.resolve());
