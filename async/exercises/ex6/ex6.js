b32744154fbddf67823b2742572cbe83d5e80012
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-31 14:38:41 -0700

    complete exercise 6

--- async/exercises/ex6/ex6.js
+++ async/exercises/ex6/ex6.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -20,8 +20,8 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	return ASQ(function(done){
-		fakeAjax(file,done);
+	return ASQ(function (done) {
+		fakeAjax(file, done);
 	});
 }
 
@@ -32,4 +32,8 @@ function getFile(file) {
 // but only once previous rendering
 // is done.
 
-// ???
+["file1", "file2", "file3"]
+	.map(getFile)
+	.reduce(function combine(chain, promise) {
+		return chain.seq(() => promise).val(output);
+	}, ASQ());
