04c59789e027c4767a1a5fdf5cd5e729273b9710
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-31 13:04:09 -0700

    complete exercise 5

--- async/exercises/ex5/ex5.js
+++ async/exercises/ex5/ex5.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -20,8 +20,19 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return ASQ(function (done) {
+		fakeAjax(file, done);
+	});
 }
+
+ASQ()
+	.seq(getFile("file1"))
+	.val(output)
+	.seq(getFile("file2"))
+	.val(output)
+	.seq(getFile("file3"))
+	.val(output)
+	.val(() => output("Complete!"));
 
 // request an array of files at once in "parallel"
 // ???
