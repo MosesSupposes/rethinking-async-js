3f9f77af90531bf395f99646bc04578809e024c0
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 16:05:27 -0700

    add missing helper functions to async-await version of exercise 7

--- async/exercises/ex7/ex7-asyncAwait.js
+++ async/exercises/ex7/ex7-asyncAwait.js
@@ -1,3 +1,22 @@
+function fakeAjax(url, cb) {
+	var fake_responses = {
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
+	};
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function () {
+		cb(fake_responses[url]);
+	}, randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
 function getFile(file) {
 	return new Promise((res, rej) => {
 		fakeAjax(file, res);
