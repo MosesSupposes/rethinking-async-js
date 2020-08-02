ec377963e96b7cf4a2de7340a387a593ad71a99a
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 15:46:46 -0700

    complete exercise 7

--- async/exercises/ex7/ex7.js
+++ async/exercises/ex7/ex7.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
+		file1: "The first text",
+		file2: "The middle text",
+		file3: "The last text",
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -20,8 +20,8 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	return ASQ(function(done){
-		fakeAjax(file,done);
+	return ASQ(function (done) {
+		fakeAjax(file, done);
 	});
 }
 
@@ -32,4 +32,23 @@ function getFile(file) {
 // but only once previous rendering
 // is done.
 
-// ???
+ASQ()
+	.runner(function* outputAllFileContents() {
+		const f1 = getFile("file1");
+		const f2 = getFile("file2");
+		const f3 = getFile("file3");
+
+		output(yield f1);
+		output(yield f2);
+		output(yield f3);
+	})
+	.val(() => output("Complete!"));
+
+//  Here's the absctraction ASQ.runner provides...
+// const it = outputAllFileContents();
+// // initialize generator to first yield
+// it.next();
+// // grab the value of the promise yielded from the generator
+// const promiseResposne = it.next().value;
+// // resume the generator, passing in the resolved promise
+// promiseResposne.then(val => it.next(val));
