89dff95f7370a6388d926729ee1732ca849203e3
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-11 15:58:48 -0700

    complete exercise 9

--- async/exercises/ex9/ex9.js
+++ async/exercises/ex9/ex9.js
@@ -1,11 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var $btn = $("#btn"),
-		$list = $("#list");
+		$list = $("#list"),
+		clicks = ASQ.csp.chan(),
+		msgs = ASQ.csp.chan(),
+		queuedClick;
 
-	$btn.click(/* TODO */);
+	$btn.on("click", listenToClicks);
 
-	// Hint: ASQ().runner( .. )
+	ASQ().runner(ASQ.csp.go(sampleClicks), ASQ.csp.go(logClick));
 
-	// TODO: setup sampling go-routine and
-	// channel, populate $list
+	function listenToClicks(evt) {
+		if (!queuedClick) {
+			queuedClick = ASQ.csp.putAsync(clicks, evt);
+			queuedClick.then(function () {
+				queuedClick = null;
+			});
+		}
+	}
+
+	function* sampleClicks() {
+		while (true) {
+			yield ASQ.csp.take(ASQ.csp.timeout(1000));
+			yield ASQ.csp.take(clicks);
+			yield ASQ.csp.put(msgs, "clicked!");
+		}
+	}
+
+	function* logClick() {
+		while (true) {
+			var msg = yield ASQ.csp.take(msgs);
+			$list.append($("<div>" + msg + "</div>"));
+		}
+	}
 });
